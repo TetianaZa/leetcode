2dcf9dbb9be092e3a5935f38464b9985c44b500f
tetiana <Lr2895921!>
2020-04-23 20:15:11 +0200

    tasks

---
+++ src/BitwiseANDofNumbersRange.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+//Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+//
+//Example 1:
+//
+//Input: [5,7]
+//Output: 4
+//Example 2:
+//
+//Input: [0,1]
+//Output: 0
+public class BitwiseANDofNumbersRange {
+
+    public static int rangeBitwiseAnd(int m, int n) {
+        int shift = 0;
+        while(m < n){ // when equals 0, stop
+            m >>= 1;
+            n >>= 1;
+            shift++;
+        }
+        return m << shift;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rangeBitwiseAnd(5,40));
+    }
+}
