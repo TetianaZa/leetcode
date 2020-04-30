df3fe72ba7137f8e9b4210a3a58247d1387c7ff1
tetiana <Lr2895921!>
2020-04-30 20:49:09 +0200

    tasks

---
+++ src/tree/CheckIfaStringIsaValidSequence.java
@@ -0,0 +1,87 @@
+package tree;
+//LeetCode
+//Explore
+//Problems
+//Mock
+//Contest
+//Articles
+//Discuss
+// Store
+// Premium
+//New Playground
+//4
+//zasadko
+//  Back to Chapter
+//  Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+//Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree.
+//
+//We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+//
+//
+//
+//Example 1:
+//
+//
+//
+//Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+//Output: true
+//Explanation:
+//The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure).
+//Other valid sequences are:
+//0 -> 1 -> 1 -> 0
+//0 -> 0 -> 0
+//Example 2:
+//
+//
+//
+//Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+//Output: false
+//Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+//Example 3:
+//
+//
+//
+//Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
+//Output: false
+//Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
+//
+//
+//Constraints:
+//
+//1 <= arr.length <= 5000
+//0 <= arr[i] <= 9
+
+public class CheckIfaStringIsaValidSequence {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public  boolean isValidSequence(TreeNode root, int[] arr) {
+        return goTree(root, arr, 0);
+    }
+
+    public boolean goTree(TreeNode node, int[] arr, int position) {
+        if (node == null || position == arr.length) return false;
+        if (node.left == null && node.right == null && node.val == arr[position] && position == arr.length - 1)
+            return true;
+        if (node.val == arr[position])
+            return goTree(node.left, arr, position + 1) || goTree(node.right, arr, position + 1);
+        return false;
+    }
+
+}
